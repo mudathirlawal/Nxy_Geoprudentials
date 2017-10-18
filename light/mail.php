388271123e79c028328a3e9488a2b8263e329db0
Mudathir Lawal <al-mudathir@live.com>
2017-10-18 10:04:33 +0100

    Latest update as at 18th, Oct, 2017. Still Working...

---
+++ light/mail.php
@@ -0,0 +1,14 @@
+<?php 
+
+$to      = 'sirolad@gmail.com'; 
+$subject = 'The test for php mail function'; 
+$message = 'Hello'; 
+$headers = 'From: test@test.com' . "\r\n" . 
+    'Reply-To: test@test.com' . "\r\n" . 
+    'X-Mailer: PHP/' . phpversion(); 
+mail($to, $subject, $message, $headers); 
+
+
+
+	<a href="index.html"><img class="" src="images/nxy-geoprudentials-logo.png" width="122" height="60" sizes="122px" alt="Nxy Geo Prudentials" /></a>
+	<div><a href="index.html"><img class="" src="images/logoSample-125-100px.jpeg" width="400" height="100" sizes="300px" alt="Nxy Geo Prudentials" /></a></div>
